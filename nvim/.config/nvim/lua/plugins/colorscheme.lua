def6e7077360928d886a125dab66c11728368bf1
LatentDream <guillaume.thibault.98@proton.me>
2025-11-16 21:26:01 -0500

    chore(nvim): fix theme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -5,22 +5,21 @@ return {
 		priority = 1000,
 		config = function()
 			vim.g.gruvbox_material_transparent_background = 1
-			vim.g.gruvbox_material_foreground = "mix"
-			vim.g.gruvbox_material_background = "hard"
-			vim.g.gruvbox_material_ui_contrast = "high"
-			vim.g.gruvbox_material_float_style = "bright"
+			vim.g.gruvbox_material_better_performance = 1
+			vim.g.gruvbox_material_foreground = "soft"
+			vim.g.gruvbox_material_background = 'soft'
+			vim.g.gruvbox_material_ui_contrast = "soft"
+			vim.g.gruvbox_material_float_style = "soft"
 			vim.g.gruvbox_material_statusline_style = "material"
 			vim.g.gruvbox_material_cursor = "auto"
+			vim.g.gruvbox_material_colors_override = {
+				-- possible key: https://github.com/sainnhe/gruvbox-material/blob/master/autoload/gruvbox_material.vim
+				green = {'#a9b665', '100'},
+				bg_green = {'#a9b665', '100'},
+				aqua =  {'#F9BE33', '100'}
+			}
 			vim.cmd.colorscheme("gruvbox-material")
 		end,
 	},
-	-- {
-	-- 	"ellisonleao/gruvbox.nvim",
-	-- 	priority = 10000,
-	-- 	lazy = false,
-	-- 	config = function()
-	-- 		vim.cmd.colorscheme("gruvbox")
-	-- 	end,
-	-- },
 	{ "thimc/gruber-darker.nvim" },
 }
